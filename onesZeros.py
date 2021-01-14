f5aca6549d1ca756273819056112edccd0b8b285
Sheikh Aafaq Rashid <sheikh.69-cse-17@mietjammu.in>
2021-01-14 12:19:22 +0530

    'solved'

---
+++ onesZeros.py
@@ -0,0 +1,11 @@
+import numpy
+
+nums =tuple(map(int,input().split()))
+
+import numpy
+
+print(numpy.zeros(nums, dtype = numpy.int)) #Type changes to int
+#Output : [[0 0]]
+
+print(numpy.ones(nums, dtype = numpy.int)) #Type changes to int
+#Output : [[1 1]]   
